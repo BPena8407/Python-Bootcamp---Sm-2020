6bb0adbc7853c958ceef8c57102b12917a01374c
Bielka Pena <bielkaestrella@hotmail.com>
2020-06-07 18:08:33 -0400

    tryal submission

---
+++ Assignment1/untitled1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 23:54:02 2020
+
+@author: bielk
+"""
+
+'''
+print("I WILL LITERALLY FIGHT YOU")
+nice = int(input("How great Bielka is? values 1-10: "))
+print(nice)
+if nice == 1:
+    print("Bielka sucks")
+elif nice == 10:
+    print("Bielka is the best")
+else:
+    print("Bielka is still better than Daniel")
+'''
